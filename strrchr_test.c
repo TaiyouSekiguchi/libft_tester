1bca2a48f0998c9f457745669dcb27d97601942f
sunix2019 <sunix.engineer.2019@gmail.com>
2021-08-18 11:32:27 +0900

    add '\0'

--- strrchr_test.c
+++ strrchr_test.c
@@ -6,7 +6,7 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 16:32:34 by tsekiguc          #+#    #+#             */
-/*   Updated: 2021/07/28 16:35:26 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2021/08/18 11:30:38 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,13 @@ char	*strrchr_test(void)
 {
 	char	*result;
 
-	result = (char *)malloc(sizeof(char) * 6);
+	result = (char *)malloc(sizeof(char) * 7);
 	result[0] = do_test("Welcome to 42Tokyo", 'e');
 	result[1] = do_test("DON'T PANIC!!", ' ');
 	result[2] = do_test("DON'T PANIC!!", '@');
 	result[3] = do_test("@@@jdifjkkkgidlsliie!!*{}\"sikggjenck\"^icsh		", '*');
 	result[4] = do_test("@@@jdifjkkkgidlsliie!!*{}\"sikggjenck\"^icsh		", '	');
-	result[5] = '\0';
+	result[5] = do_test("@@@jdifjkkkgidlsliie!!*{}\"sikggjenck\"^icsh		", '\0');
+	result[6] = '\0';
 	return (result);
 }
